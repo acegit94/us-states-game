298e053e6074f76d143620532233b4b403fbf23c
ace <acegit94@gmail.com>
2025-06-26 13:33:10 +0100

    Fixed accepting duplicate countries as answer bug

--- main.py
+++ main.py
@@ -18,10 +18,10 @@ US_STATES = pandas.read_csv("./50_states.csv")
 def get_state_details(answer: str) -> Optional[Tuple[str, int, int]]:
     """Return state name and coordinates if exists
     otherwise return none"""
-    us_state = US_STATES[US_STATES.state == answer]
+
+    us_state = US_STATES[US_STATES.state == answer.title()]
     if us_state.empty:
         return None
-
     name = us_state.iloc[0].state
     x = us_state.iloc[0].x
     y = us_state.iloc[0].y
@@ -38,12 +38,11 @@ def fill_map(name: str, x: int, y: int):
 def missed_states_csv(all_states: list, guessed_states: list):
     """Create csv with missing states after game over"""
     missing_states = []
-    for state in all_states:
-        if state not in guessed_states:
-            missing_states.append(state)
+    missing_states = [state for state in all_states if state not in guessed_states]
 
-    missed_data = pandas.DataFrame(missing_states)
-    missed_data.to_csv("./missed.csv", index = False)
+    if missing_states:
+        missed_data = pandas.DataFrame(missing_states)
+        missed_data.to_csv("./missed.csv", index=False)
 
 def game_logic():
     """Game logic"""
@@ -52,15 +51,17 @@ def game_logic():
     guessed_states = []
 
     while correct_guesses <= 50:
-        answer_state = screen.textinput(title=f"{correct_guesses}/50 States Correct", prompt="What's another state name").title()
-        if answer_state == "Exit":
+        answer_state = screen.textinput(title=f"{correct_guesses}/50 States Correct", prompt="What's another state name")
+        if answer_state == "Exit" or answer_state is None:
             break
         if get_state_details(answer_state) is None:
             continue
+
         name, x, y = get_state_details(answer_state)
-        fill_map(name, x, y)
-        correct_guesses += 1
-        guessed_states.append(name)
+        if name not in guessed_states:
+            fill_map(name, x, y)
+            correct_guesses += 1
+            guessed_states.append(name)
 
     missed_states_csv(all_states, guessed_states)
 
